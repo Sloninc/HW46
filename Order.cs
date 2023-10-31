5ba2d9fe682fdf473d14475f90a1f4e8f1d496f7
Andrei Ruk <sloninc@mail.ru>
2023-10-31 15:25:41 +0300

    Добавлен класс Order

---
+++ Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW46
+{
+    public class Order
+    {
+        public Order() { }
+        public Order((int, int, int) order)
+        {
+            CustomerId = order.Item1;
+            ProductId = order.Item2;
+            Quantity = order.Item3;
+        }
+
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
