fd0f1cfab76ee99ac7ec772f25ccc20a724910d2
Andrei Ruk <sloninc@mail.ru>
2023-10-31 18:16:34 +0300

    финальная версия

---
+++ PplOverThirtyBuyProdOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW46
+{
+    internal class PplOverThirtyBuyProdOne
+    {
+        public int CustomerId { get; set; }
+        public string? FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
-﻿
+﻿using Dapper;
+using Npgsql;
 
 namespace HW46
 {
     public class Program
     {
         public const string ConnectionString =
-            "Server=localhost;port=5432; DataBase=shop; UserId =postgres; password=AvisaT53@;";
+            "Server=localhost;port=5432; DataBase=Shop; UserId =postgres;";
+        public const string ConnectionString2 =
+                    "Server=localhost;port=5432; DataBase=Shop2; UserId =postgres;";
 
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Введите Id покупателя");
             var customerById = GetCustomerById(Console.ReadLine());
@@ -19,121 +22,140 @@ namespace HW46
                 Console.WriteLine($"id - {customer.Id} name - {customer.FirstName}, lastname - {customer.LastName}, age - {customer.Age}");
             }
 
-            Console.WriteLine("Это были задания до 4.");
+            Console.WriteLine("Задание 4.");
             Console.ReadKey();
-            Console.WriteLine("Приступаем к 4 заданию");
-            Console.WriteLine("Создаем копии таблицы(приставка 2)");
-            using (var connection = new NpgsqlConnection(ConnectionString))
+            using (var connection = new NpgsqlConnection(ConnectionString2))
             {
                 var command = new NpgsqlCommand(string.Empty, connection);
                 command.CommandText = @"
-CREATE TABLE Customers2 (
-ID SERIAL PRIMARY KEY,
-FirstName VARCHAR(50),
-LastName VARCHAR(50),
-Age INTEGER);
-CREATE TABLE Products2 (
-ID SERIAL PRIMARY KEY,
-Name VARCHAR(50),
-Description VARCHAR(255),
-StockQuantity INTEGER,
-Price DECIMAL(10, 2)
-);
-CREATE TABLE Orders2 (
-ID SERIAL PRIMARY KEY,
-CustomerID INTEGER REFERENCES Customers2(ID),
-ProductID INTEGER REFERENCES Products2(ID),
-Quantity integer);";
+                    CREATE TABLE Customers (
+                    ID SERIAL PRIMARY KEY,
+                    FirstName VARCHAR(50),
+                    LastName VARCHAR(50),
+                    Age INTEGER);
+                    CREATE TABLE Products (
+                    ID SERIAL PRIMARY KEY,
+                    Name VARCHAR(50),
+                    Description VARCHAR(255),
+                    StockQuantity INTEGER,
+                    Price DECIMAL(10, 2)
+                        );
+                    CREATE TABLE Orders (
+                    ID SERIAL PRIMARY KEY,
+                    CustomerID INTEGER REFERENCES Customers(ID),
+                    ProductID INTEGER REFERENCES Products(ID),
+                    Quantity integer);";
                 connection.Open();
                 command.ExecuteNonQuery();
             }
             var customersArray = new[] {
-                ("John", "Doe", 35),
-                ("Jane", "Smith", 28),
-                ("Michael", "Johnson", 42),
-                ("Emily", "Davis", 31),
-                ("Daniel", "Wilson", 39),
-                ("Olivia", "Anderson", 26),
-                ("William", "Brown", 34),
-                ("Sophia", "Taylor", 37),
-                ("James", "Miller", 29),
-                ("Ava", "Martinez", 33)
+                ("Никифоров", "Авраам", 34),
+                ("Архипов", "Петр", 56),
+                ("Семёнов", "Семёнов", 23),
+                ("Лихачёв", "Вальтер", 30),
+                ("Агафонов", "Мирослав", 19),
+                ("Панов", "Клим", 45),
+                ("Мухин", "Адольф", 38),
+                ("Беспалов", "Казимир", 36),
+                ("Туров", "Богдан", 45),
+                ("Богданов", "Василий", 37),
+                ("Шестаков", "Даниил", 31),
+                ("Сысоев", "Гавриил", 57),
+                ("Николаев", "Давид", 34),
+                ("Панов", "Богдан", 46),
+                ("Кабанов", "Гордей", 38),
+                ("Чернов", "Исак", 39),
+                ("Корнилов", "Тарас", 40),
+                ("Тихонов", "Эдуард", 50),
+                ("Белозёров", "Гарри", 31),
+                ("Гаврилов", "Петр", 33)
             };
             var newCustomers = CreateCustomers(customersArray);
             var productArray = new[]
             {
-                ("Product1", "Description1", 10, 9.99),
-                ("Product2", "Description2", 5, 19.99),
-                ("Product3", "Description3", 15, 14.99),
-                ("Product4", "Description4", 8, 24.99),
-                ("Product5", "Description5", 12, 11.99),
-                ("Product6", "Description6", 3, 29.99),
-                ("Product7", "Description7", 20, 7.99),
-                ("Product8", "Description8", 6, 16.99),
-                ("Product9", "Description9", 18, 13.99),
-                ("Product10", "Description10", 9, 22.99)
+                ("Яйца", "Синявинская птицефабрика", 1000, 89.00),
+                ("Чеснок", "Китай", 60, 46.00),
+                ("Сахар", "Тростниковый Кубинский", 985, 120.00),
+                ("Пудра сахарная", "Для десертов", 580, 86.00),
+                ("Сливки", "Пискаревский завод", 470, 180.00),
+                ("Масло растительное", "Дары кубани", 730, 78.00),
+                ("Куриное филе", "Синявинская птицефабрика", 800, 230.00),
+                ("Приправа", "Адыгейская", 60, 132.00),
+                ("Лук репчатый", "Фирма Лето", 790, 34.00),
+                ("Майонез", "На перепелиных яйцах", 670, 101.00)
             };
             var newProducts = CreateProducts(productArray);
             var ordersArray = new[]
             {
-                (1, 1, 2),
-                (2, 3, 1),
-                (3, 2, 3),
-                (4, 5, 2),
-                (5, 4, 1),
-                (6, 7, 4),
-                (7, 10, 2),
-                (8, 8, 1),
-                (9, 6, 3),
-                (10, 9, 2)
+                (1, 1, 10),
+                (2, 10, 1),
+                (3, 9, 2),
+                (4, 7, 4),
+                (5, 8, 1),
+                (6, 6, 2),
+                (7, 3, 3),
+                (8, 2, 4),
+                (9, 6, 1),
+                (10, 4, 1),
+                (11, 4, 2),
+                (12, 6, 1),
+                (13, 9, 1),
+                (14, 5, 2),
+                (15, 10, 4),
+                (16, 7, 5),
+                (17, 3, 2),
+                (18, 1, 30),
+                (19, 2, 3),
+                (20, 8, 2)
             };
             var newOrders = CreateOrders(ordersArray);
             InsertCustomers(newCustomers);
             InsertProducts(newProducts);
             InsertOrders(newOrders);
-            using (var connection = new NpgsqlConnection(ConnectionString))
+            using (var connection = new NpgsqlConnection(ConnectionString2))
             {
                 var command = new NpgsqlCommand(string.Empty, connection);
-                command.CommandText = "CREATE INDEX idx_orders_productid2 ON Orders2(ProductID);";
+                command.CommandText = "CREATE INDEX idx_orders_productid ON Orders(ProductID);";
                 connection.Open();
                 command.ExecuteNonQuery();
             }
 
-            var result = GetOldManWhoBuyFirstProduct();
-            foreach (var oldManWhoBuyFirstProduct in result)
+            var result = GetPplOverThirtyBuyProdOne();
+            foreach (var pplOverThirtyBuyProdOne in result)
             {
 
                 Console.WriteLine($@"
-result:
-{oldManWhoBuyFirstProduct.CustomerId} - customerId
-{oldManWhoBuyFirstProduct.FirstName} - firstName
-{oldManWhoBuyFirstProduct.LastName} - lastName
-{oldManWhoBuyFirstProduct.ProductId} - productId
-{oldManWhoBuyFirstProduct.Quantity} - quantity
-{oldManWhoBuyFirstProduct.Price} - price 
-");
+                    result:
+                    {pplOverThirtyBuyProdOne.CustomerId} - customerId
+                    {pplOverThirtyBuyProdOne.FirstName} - firstName
+                    {pplOverThirtyBuyProdOne.LastName} - lastName
+                    {pplOverThirtyBuyProdOne.ProductId} - productId
+                    {pplOverThirtyBuyProdOne.Quantity} - quantity
+                    {pplOverThirtyBuyProdOne.Price} - price 
+                    ");
             }
+            Console.ReadLine();
         }
 
-        private static IEnumerable<OldManWhoBuyFirstProduct> GetOldManWhoBuyFirstProduct()
+        private static IEnumerable<PplOverThirtyBuyProdOne> GetPplOverThirtyBuyProdOne()
         {
             using var connection = new NpgsqlConnection(ConnectionString);
             connection.Open();
 
             const string sql = @"
-        SELECT c.ID AS CustomerID, c.FirstName, c.LastName, o.ProductID, o.Quantity AS ProductQuantity, p.Price AS ProductPrice
-        FROM Customers c
-        JOIN Orders o ON c.ID = o.CustomerID
-        JOIN Products p ON o.ProductID = p.ID
-        WHERE c.Age > 30 AND o.ProductID = 1";
+                SELECT c.ID AS CustomerID, c.FirstName, c.LastName, o.ProductID, o.Quantity AS ProductQuantity, p.Price AS ProductPrice
+                FROM Customers c
+                JOIN Orders o ON c.ID = o.CustomerID
+                JOIN Products p ON o.ProductID = p.ID
+                WHERE c.Age > 30 AND o.ProductID = 1";
 
-            return connection.Query<OldManWhoBuyFirstProduct>(sql);
+            return connection.Query<PplOverThirtyBuyProdOne>(sql);
         }
 
         private static void InsertCustomers(IEnumerable<Customer> newCustomers)
         {
             using var connection = new NpgsqlConnection(ConnectionString);
-            const string commandText = @"insert into Customers2 (FirstName, LastName, Age) values (@FirstName, @LastName, @Age);";
+            const string commandText = @"insert into Customers (FirstName, LastName, Age) values (@FirstName, @LastName, @Age);";
             foreach (var customer in newCustomers)
             {
                 connection.Execute(commandText, new { customer.FirstName, customer.LastName, customer.Age });
@@ -142,7 +164,7 @@ result:
         private static void InsertProducts(IEnumerable<Product> newProducts)
         {
             using var connection = new NpgsqlConnection(ConnectionString);
-            const string commandText = @"insert into Products2 (Name, Description, StockQuantity, Price) values (@Name, @Description, @StockQuantity, @Price);";
+            const string commandText = @"insert into Products (Name, Description, StockQuantity, Price) values (@Name, @Description, @StockQuantity, @Price);";
             foreach (var product in newProducts)
             {
                 connection.Execute(commandText, new { product.Name, product.Description, product.StockQuantity, product.Price });
@@ -151,7 +173,7 @@ result:
         private static void InsertOrders(IEnumerable<Order> newOrders)
         {
             using var connection = new NpgsqlConnection(ConnectionString);
-            const string commandText = @"insert into Orders2 (CustomerId, ProductId, Quantity) values (@CustomerId, @ProductId, @Quantity);";
+            const string commandText = @"insert into Orders (CustomerId, ProductId, Quantity) values (@CustomerId, @ProductId, @Quantity);";
             foreach (var order in newOrders)
             {
                 connection.Execute(commandText, new { order.CustomerId, order.ProductId, order.Quantity });
@@ -184,7 +206,7 @@ result:
         }
 
 
-        public static IEnumerable<Customer> GetAllCustomers()
+        private static IEnumerable<Customer> GetAllCustomers()
         {
             using var connection = new NpgsqlConnection(ConnectionString);
             const string commandText = @"select Id, Firstname, Lastname, Age from Customers;";
