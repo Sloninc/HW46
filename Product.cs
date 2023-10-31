ad57e4668f66a931665f3b37dc30895ea26073a4
Andrei Ruk <sloninc@mail.ru>
2023-10-31 15:26:59 +0300

    Добавлен класс Product

---
+++ Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW46
+{
+    public class Product
+    {
+        public Product() { }
+        public Product((string, string, int, double) product)
+        {
+            Name = product.Item1;
+            Description = product.Item2;
+            StockQuantity = product.Item3;
+            Price = (decimal)product.Item4;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int StockQuantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
